ff8b0073e0ff6cca1b6c490302e1b733c0a480ce
Mickael Sermont <sermont.mickael@hotmail.fr>
2022-10-06 14:37:12 +0200

    feat: done barchart with data & styles

---
+++ src/components/Profile/Charts/BarChart/index.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Bar,
+  BarChart as BarGraph,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import styles from "styles/components/profile/charts.module.scss";
+
+const CustomTooltip = ({ active, payload }) => {
+  console.log(payload);
+  if (active && payload && payload.length) {
+    return (
+      <div className={styles.customTooltip}>
+        <p>{`${payload[0].value}${payload[0].unit}`}</p>
+        <p>{`${payload[1].value}${payload[1].unit}`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+function BarChart({ data }) {
+  return (
+    <div className={styles.barChartContainer}>
+      <div className={styles.headerBarChart}>
+        <h3 className={styles.titleHeader}>Activité quotidienne</h3>
+
+        <div className={styles.legendContainer}>
+          <div className={styles.legendWrapper}>
+            <div className={`${styles.roundLegend} ${styles.weight}`}></div>
+            <h4>Poids (kg)</h4>
+          </div>
+          <div className={styles.legendWrapper}>
+            <div className={`${styles.roundLegend} ${styles.calories}`}></div>
+            <h4>Calories brûlées (kCal)</h4>
+          </div>
+        </div>
+      </div>
+
+      <ResponsiveContainer width={750} height="100%">
+        <BarGraph data={data}>
+          <CartesianGrid vertical={false} strokeDasharray="2 2" />
+          <XAxis
+            dataKey="day"
+            tickLine={false}
+            tick={{ fontSize: 14, fontWeight: 500, fill: "#9B9EAC" }}
+            dy={15}
+            stroke="1 1"
+          />
+          <YAxis
+            yAxisId="kilogram"
+            dataKey="kilogram"
+            type="number"
+            domain={["dataMin-2", "dataMax+5"]}
+            tickCount="3"
+            orientation="right"
+            axisLine={false}
+            tickLine={false}
+            tick={{ fontSize: 14, fontWeight: 500, fill: "#9B9EAC" }}
+            dx={15}
+          />
+          <YAxis
+            yAxisId="calories"
+            dataKey="calories"
+            type="number"
+            hide={true}
+          />
+
+          <Tooltip content={<CustomTooltip />} />
+
+          <Bar
+            yAxisId="kilogram"
+            dataKey="kilogram"
+            unit="kg"
+            fill="var(--darkGray-color)"
+            barSize={7}
+            radius={[50, 50, 0, 0]}
+          />
+          <Bar
+            yAxisId="calories"
+            dataKey="calories"
+            unit="Kcal"
+            fill="var(--primary-color)"
+            barSize={7}
+            radius={[50, 50, 0, 0]}
+          />
+        </BarGraph>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+BarChart.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    day: PropTypes.string.isRequired,
+    kilogram: PropTypes.number.isRequired,
+    calories: PropTypes.number.isRequired,
+})),
+};
+
+export default BarChart;
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,24 +1,30 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 import data from "data/mocks.json";
+import { useParams } from "react-router-dom";
 import styles from "styles/components/profile.module.scss"
 
 import UserInfo from "components/Profile/UserInfo";
 import StatsList from "components/Profile/Stats/StatsList";
+import BarChart from "components/Profile/Charts/BarChart";
 
 function Profile() {
   // get ID from url
-  const params = useParams();
+  // const params = useParams();
 
+  const userStats = data.data.keyData;
+  const userActivity = data.activity.data.sessions;
 
   return (
     <div className={styles.profileContainer}>
       <UserInfo firstname={"Thomas"} />
 
       <div className={styles.profileGrid}>
-        <div className={styles.graphsColumn}>diagramme</div>
+        <div className={styles.graphsColumn}>
+          <BarChart data={userActivity} />
+        </div>
+
         <div className={styles.statsColumn}>
-          <StatsList stats={data.data.keyData} />
+          <StatsList stats={userStats} />
         </div>
       </div>
     </div>
